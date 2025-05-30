154d190228348ee866b386d4c23d280ed008554e
Vishal.R.Nadagoudar <137093870+VishalRN@users.noreply.github.com>
2025-05-30 23:50:16 +0530

    yes

---
+++ index57.js
@@ -0,0 +1,42 @@
+// 57 - JavaScript Loops
+
+let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+//for looop 
+// for (let i = 0; i < 100; i++) { 
+//     console.log(a + i); 1 - 100
+// }
+
+//for in loop
+// let obj = {
+//     name: "Vishal",
+//     role: "Programmer",
+//     company: "Google"
+// }
+
+// for (const key in obj) {
+//     console.log(key, element) both 
+// }
+
+// for of loop
+// for (const c of "Vishal") {
+//         console.log(c) each charecter seperately 
+//     }
+
+// while loop
+// let i = 0;
+// while (i < 60000) {
+//     console.log(i)
+//     i++;
+// }
+
+//do while (the only loop that executes atleast once completely)
+let i = 10;
+do {
+    console.log(i) //10
+    i++;
+} while (i < 6);
+
