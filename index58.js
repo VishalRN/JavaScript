7f37e5a8dc86e2efaf73e54a4c6d092dd15bfaa7
Vishal.R.Nadagoudar <137093870+VishalRN@users.noreply.github.com>
2025-05-31 22:39:37 +0530

    yes

---
+++ index58.js
@@ -0,0 +1,34 @@
+// 58 - JavaScript Functions
+
+function nice(name) {
+    console.log("Hey " + name + " you are nice!")
+    console.log("Hey " + name + " you are good!")
+    console.log("Hey " + name + " your tshirt is nice!")
+    console.log("Hey " + name + " your course is good too!")
+}
+
+nice("Rohan")
+nice("Vishal")
+
+function sum(a, b, c = 3) {
+    console.log(a, b, c)
+    return a + b + c
+}
+
+result1 = sum(3, 2)
+result2 = sum(7, 5)
+result3 = sum(3, 13, 1) // c =1
+
+console.log("The sum of these numbers is: ", result1)
+console.log("The sum of these numbers is: ", result2)
+console.log("The sum of these numbers is: ", result3)
+
+// Arrow Function
+const func1 = (x) => {
+    console.log("I am an arrow function", x) //it can be reused
+}
+
+func1(34);
+func1(66);
+func1(84);
+
