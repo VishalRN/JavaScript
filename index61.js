a5855b66f2a4d78a20565f7f0d670913fa98bdea
Vishal.R.Nadagoudar <137093870+VishalRN@users.noreply.github.com>
2025-06-16 22:37:00 +0530

    yes

--- index61.js
+++ index61.js
@@ -13,4 +13,32 @@ It performs wrong operation 10% of the times
 
 */
 
+// Input Numbers
+let a = Number(prompt("Enter your number 1 "));
+let b = Number(prompt("Enter your number 2 "));
+// Random Number
+
+let random = Math.random();
+
+let addition = a+b ;
+let Subtraction = a-b;
+let Multiplication =a*b;
+let Division= a/b;
+let Exponentiation = a**b;
+
+// Condition
+if (random <0.1){
+    console.log("Addition Calculation : " , a-b) ;
+    console.log("Subtraction Calculation : " , a/b) ;
+    console.log("Multiplication Calculation : " , a+b );
+    console.log("Division Calculation : " ,a**b ) ;
+}
+
+else{ 
+    console.log("Addition Calculation : " , a+b) ;
+    console.log("Subtraction Calculation : " , a-b) ;
+    console.log("Multiplication Calculation : " , a*b );
+    console.log("Division Calculation : " ,a**b ) ;
+}
+
 
